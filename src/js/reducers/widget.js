3a84dab59edd837edf7d6ff799be02a5f692a8f1
Max Patsiansky <m.patsianskiy@zmeke.com>
2015-08-24 12:01:24 +0300

    fake turn in_progress -> suspend

--- src/js/reducers/widget.js
+++ src/js/reducers/widget.js
@@ -36,14 +36,36 @@ export default function widget(state = initialState, action) {
   switch (action.type) {
     case CHANGE_STATUS:
       let task = action.task;
+      task.status = action.status_id;
+
       let nextTaskQueue = state.tasksQueue;
 
-      if (task.id !== nextTaskQueue[0].id) {
+      /*if (task.id !== nextTaskQueue[0].id) {
         _.remove(nextTaskQueue,{id: task.id});
         task.status = action.status_id;
         nextTaskQueue.unshift(task);
       } else {
         nextTaskQueue[0].status = action.status_id;
+      }*/
+
+
+
+      //текущий таск = tasksQueue[0]
+      //Task остается текущим, если нет никакого другого таска в статусе in_progress
+
+      let taskInProgress = _.remove(nextTaskQueue, (item) =>{
+        return item.status === status.IN_PROGRESS;
+      });
+
+      //если текущий task находится в in_progress, и таску из очереди присваивается in_progress,
+      //то текущий таск автоматически переводится в suspend, и перемещается в верх очереди.
+      //для запроса реальной очереди задач (по приоритету и прочему) - необходимо "обновить очередь"
+      if (taskInProgress.length > 1) {
+        taskInProgress[0].status = status.SUSPEND;
+        nextTaskQueue.unshift(taskInProgress[0]);
+        nextTaskQueue.unshift(taskInProgress[1]);
+      } else if (taskInProgress.length === 1) {
+        nextTaskQueue.unshift(taskInProgress[0]);
       }
 
       return {...state, tasksQueue: nextTaskQueue};
